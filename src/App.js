c399baaee96d23f7675cc04834e1ef9bf6d5f23e
Lisa Pena <lisapena13@gmail.com>
2020-09-22 12:17:09 -0500

    Inital js and components generated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Login from "./components/Login";
+import data from "./Data";
+import Context from "./Context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+
+    };
+
+    this.routerRef = React.createRef();
+  }
+
+  login = (usn, pwd) => {
+    let user = data.users.find(u => u.username === usn && u.password === pwd);
+    if (user) {
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    }
+    return false;
+  };
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className="App">
+            <nav
+              className="navbar container"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div className="navbar-brand">
+                <b className="navbar-item is-size-4 ">ecommerce</b>
+                <a
+                  role="button"
+                  class="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>
+              </div>
+              <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    {Object.keys(this.state.cart).length}
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
+                ) : (
+                  <a className="navbar-item" onClick={this.logout}>
+                    Logout
+                  </a>
+                )}
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={Component} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Component} />
+              <Route exact path="/add-product" component={Component} />
+              <Route exact path="/products" component={Component} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
 }
 
-export default App;
+
